8bd7e47d82ccdcdf5e15a33083239001432adc03
MoLi7 <mo.vico.li@gmail.com>
2022-03-02 13:05:32 -0800

    Improve install_useeior to avoid repeated installation

--- R/utils.R
+++ R/utils.R
@@ -2,9 +2,16 @@
 #' @param SEF_version A string of SEF version, e.g. "v1.1.1".
 install_useeior <- function(SEF_version) {
   versioning <- configr::read.config("Versioning.yml")
-  useeior_version <- versioning[[SEF_version]][["useeior"]]
-  if (!"devtools"%in%installed.packages()[, "Package"]) {
+  useeior_tag <- versioning[[SEF_version]][["useeior_tag"]]
+  useeior_ver <- versioning[[SEF_version]][["useeior_ver"]]
+  installed_pkg <- installed.packages()
+  if (!"devtools"%in%installed_pkg[, "Package"]) {
     install.packages("devtools")
   }
-  devtools::install_github(paste0("USEPA/useeior@", useeior_version))
+  if (!"useeior"%in%installed_pkg[, "Package"]) {
+    devtools::install_github(paste0("USEPA/useeior@", useeior_tag))
+  }
+  if ("useeior"%in%installed_pkg[, "Package"] && useeior_ver!=installed_pkg[installed_pkg[, "Package"]=="useeior", "Version"]) {
+    devtools::install_github(paste0("USEPA/useeior@", useeior_tag))
+  }
 }

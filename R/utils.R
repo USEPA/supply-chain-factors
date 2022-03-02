1d2b206a8fcfb6fa582072ef9cdd4f82cec2f31e
MoLi7 <mo.vico.li@gmail.com>
2022-03-02 14:52:02 -0800

    Update install_useeior function - Use here::here to locate Versioning.yml in root directory - Add loginfo and logwarn to notify users about installing useeior

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 #' Install useeior (via devtools) for specified SEF version.
 #' @param SEF_version A string of SEF version, e.g. "v1.1.1".
 install_useeior <- function(SEF_version) {
-  versioning <- configr::read.config("Versioning.yml")
+  versioning <- configr::read.config(here::here("Versioning.yml"))
   useeior_tag <- versioning[[SEF_version]][["useeior_tag"]]
   useeior_ver <- versioning[[SEF_version]][["useeior_ver"]]
   installed_pkg <- installed.packages()
@@ -9,9 +9,14 @@ install_useeior <- function(SEF_version) {
     install.packages("devtools")
   }
   if (!"useeior"%in%installed_pkg[, "Package"]) {
+    logging::loginfo(paste0("Installing useeior v", useeior_ver, " (tag @", useeior_tag, ") from GitHub..."))
     devtools::install_github(paste0("USEPA/useeior@", useeior_tag))
   }
   if ("useeior"%in%installed_pkg[, "Package"] && useeior_ver!=installed_pkg[installed_pkg[, "Package"]=="useeior", "Version"]) {
+    logging::logwarn(paste0("A new version of useeior (v", useeior_ver, ") will be installed for generating SEF ", SEF_version,
+                            ". The useeior v", installed_pkg[installed_pkg[, "Package"]=="useeior", "Version"],
+                            " you have installed will be overwritten."))
+    logging::loginfo(paste0("Installing useeior v", useeior_ver, " (tag @", useeior_tag, ") from GitHub..."))
     devtools::install_github(paste0("USEPA/useeior@", useeior_tag))
   }
 }

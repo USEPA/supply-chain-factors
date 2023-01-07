16aec9f45f6fc1cd6da65fff5c3d84442d55f431
WesIngwersen <ingwersen.wesley@epa.gov>
2023-01-06 21:03:43 -0500

    CalculateEmissionsFactors like code + Functions update to calculate USEEIOv2.1.19 only

--- R/Functions.R
+++ R/Functions.R
@@ -1,5 +1,5 @@
 #' Generate emission factor table that includes Supply-chain Emission Factor (SEF)
-#' and Marginal Emission Factor (MEF) of CO2, CH4, N2O, and other GHGs in 2018 USD
+#' and Marginal Emission Factor (MEF) of GHGs
 #' for US commodities/industries at the BEA Detail/Summary level of categorization.
 #' @param model A complete USEEIO model.
 #' @param price_adjusted_model A list of price-adjusted matrices from the USEEIO model.
@@ -9,19 +9,22 @@ generateEmissionFactorTable <- function(model, price_adjusted_model, margin = FA
   dollaryear <- unique(sub(".*\\_", "", names(price_adjusted_model)))
   # Generate supply chain emission factors table
   if (margin) {
-    M_matrix <- paste0("M_margin_pur_", dollaryear)
+    #M_matrix <- paste0("M_margin_pur_", dollaryear)
     N_matrix <- paste0("N_margin_pur_", dollaryear)
   } else {
-    M_matrix <- paste0("M_pur_", dollaryear)
+    #M_matrix <- paste0("M_pur_", dollaryear)
     N_matrix <- paste0("N_pur_", dollaryear)
   }
-  df <- rbind(as.data.frame(price_adjusted_model[[M_matrix]],
-                            row.names = gsub("/.*", "", rownames(price_adjusted_model[[M_matrix]]))),
-              as.data.frame(price_adjusted_model[[N_matrix]])["Other Greenhouse Gases", ])
+  #df <- rbind(as.data.frame(price_adjusted_model[[M_matrix]],
+  #                          row.names = gsub("/.*", "", rownames(price_adjusted_model[[M_matrix]]))),
+  #            as.data.frame(price_adjusted_model[[N_matrix]])["Other Greenhouse Gases", ])
+  df <- as.data.frame(price_adjusted_model[[N_matrix]])["GWP-AR4-100", ]
   df$Unit <- paste0(c(rep(paste0("kg/", dollaryear, " USD"), nrow(df)-1),
                       paste0("kg CO2e/", dollaryear, " USD")),
                     ", purchaser price")
-  df$Flowable <- rownames(df)
+  #df$Flowable <- rownames(df)
+  df$Flowable <- "CO2e"
+  
   # Reshape df into long table
   df_long <- reshape2::melt(df, id.vars = c("Flowable", "Unit"),
                             variable.name = "Sector", value.name = "FlowAmount")
